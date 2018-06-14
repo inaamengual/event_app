f45c28dd9928e042dbc31c0c7642f60d57344c51
Andy Weiss <andyweiss1982@gmail.com>
2018-06-13 20:43:33 -0400

    double model devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,30 @@
 Rails.application.routes.draw do
-  root to: 'welcome#index'
-
-  authenticated :user do
-    root to: "companies#index", as: :authenticated_user_root
+  # devise routes, keep in this order ###################
+  devise_scope :user do
+    authenticated :user do
+      root                to: "companies#index",
+                          as: :authenticated_user_root
+    end
   end
-
-  authenticated :company do
-    root to: "companies#show", as: :authenticated_company_root
+  devise_scope :company do
+    authenticated :company do
+      root                to: "companies#show",
+                          as: :authenticated_company_root
+    end
   end
-
-  devise_for :companies,
-    path: 'companies',
+  devise_for :companies,  path: "companies",
     controllers: {
-      sessions: 'companies/sessions',
-      registrations: 'companies/registrations',
-      passwords: 'companies/passwords'
+      sessions:           "companies/sessions",
+      registrations:      "companies/registrations",
+      passwords:          "companies/passwords"
     }
-
-  devise_for :users,
-    path: 'users',
+  devise_for :users,      path: "users",
     controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations',
-      passwords: 'users/passwords'
+      sessions:           "users/sessions",
+      registrations:      "users/registrations",
+      passwords:          "users/passwords"
     }
-
+  root                    to: "users/sessions#new"
+  # non-devise routes ####################################
   resources :companies, only: [:index, :show]
-
-  unauthenticated :user do
-    root to: "users/sessions#new"
-  end
-
-  unauthenticated :company do
-    root to: "companies/sessions#new"
-  end
-
-
 end
